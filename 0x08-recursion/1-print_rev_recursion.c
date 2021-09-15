abfcfd77c1e73193b84d96ba0e69821d77fe5ac2
abrsh21 <abrhamgetachew625@gmail.com>
2021-09-15 03:59:22 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - return length of a string
+ * _print_rev_recursion - print a string in reverse
  * @s: string
- * Return: length of string
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	int c;
-
-	c = 0;
 	if (*s == '\0')
-		return (0);
+		return;
 
-	c = 1 +  _strlen_recursion(s + 1);
-
-	return (c);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
