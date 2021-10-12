ea4a9764bc1bb434ef75ae618bd883387f230f13
abrsh21 <abrhamgetachew625@gmail.com>
2021-10-12 13:52:53 -0700

    santos

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
-/**
- * delete_nodeint_at_index - thsi function delete a node
- * @head: the head of list
+i function delete a node
+~ * @head: the head of list
  * @index: the index to put the delete node
  * Description: this function delete a node
  * section header: the header of this function is lists.h)*
